1dac372cc64650d95a625a183fe0449076b881fc
melinakremer <melnatal25@icloud.com>
2022-01-11 16:47:47 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form() {
+    return (
+        <h1>
+            pagina de form
+        </h1>
+    );
+}
+export default Form;
---
+++ frontend/src/pages/Listing/index.jsx
@@ -0,0 +1,8 @@
+function Listing() {
+    return (
+        <h1>
+            pagina de Listagem
+        </h1>
+    );
+}
+export default Listing;git
